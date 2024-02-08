376c40aed938858d3700a782bac740ca16474a4a
zindev <alissonludtkeschwanz@gmail.com>
2024-02-08 09:13:19 -0400

    fix result

--- components/manor-calculator/result.tsx
+++ components/manor-calculator/result.tsx
@@ -3,7 +3,7 @@ import { useManorCalculator } from "./useManorCalculator"
 import { formatName } from "./utils"
 
 export function Result() {
-  const { material, quantity } = useManorCalculator()
+  const { material, result } = useManorCalculator()
   return (
     <div className="relative z-0 flex items-center gap-2">
       <Image
@@ -15,7 +15,7 @@ export function Result() {
         className="absolute left-2 -z-10 rounded-full blur-md"
       />
       <p className="text-lg">
-        <span className="font-mono">{quantity}</span> {material}
+        <span className="font-mono">{result}</span> {material}
       </p>
       <Image
         src={`/materials/${formatName(material)}.png`}
